945bdd4a60b32a72680662a972b4f65f3b2a111d
Kevin Leskinen <kevnl_95@hotmail.com>
2021-03-19 09:19:03 +0100

    added line

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer>
+            <hr />
             <p>Copyright moviebase</p>
         </footer>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div>
             <h1 className="title">MovieBase</h1>
+            <hr />
         </div>
     )
 }
